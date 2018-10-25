2846f54f0bf164b931c22c93d0bf06e8e2c2e4a6
dadadamarine <dadadamarine@naver.com>
2018-10-26 01:51:37 +0900

    JSON 활용한 Ajax처리

--- nodeServerVimeo/app.js
+++ nodeServerVimeo/app.js
@@ -49,6 +49,8 @@ app.post('/email_post', (req, res)=>{
 });
  */
 
+
+
 //html + data를 합쳐서 전달하기 
 // 그러기 위해 ejs 모듈 설치
 app.post('/email_post', (req, res)=>{
@@ -57,4 +59,14 @@ app.post('/email_post', (req, res)=>{
     console.log(req.body.email); // email주소가 출력됨.
     //res.send("<h1>welcome! "  + req.body.email +"</h1>");
     res.render('email.ejs' , {'email' : req.body.email}); // 뒤에 오브젝트를 넣어줌 , email이라고 적힌애들을 ejs에 찾아서 치환하여 클라이언트로 응답 넘김.
+});
+
+
+
+
+// ajax : 웹 페이지 전체를 다시 로딩하지 않고도, 웹 페이지의 일부분만을 갱신할 수 있습니다.
+app.post('/ajax_send_email', function(req, res){
+    console.log(req.body.email);
+    var responserData = {'result': 'ok' , 'email' : req.body.email};
+    res.json(responserData);
 });
