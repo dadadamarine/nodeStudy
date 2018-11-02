fca7bf3fd84dd1915006cdca4f0992faba62fac1
dadadamarine <dadadamarine@naver.com>
2018-11-03 01:35:58 +0900

    Restful Api의 기초 :  get = Retrieve (리스트 가져오기) , movie/list

--- nodeServerVimeo/router/movie/index.js
+++ nodeServerVimeo/router/movie/index.js
@@ -20,5 +20,21 @@ router.get('/list', (req,res)=>{
 	res.render('movie.ejs');
 })
 
+//1. /movie , GET
+router.get('/', (req,res)=>{
+	var responseData = {};
+
+	var query = connection.query('select title from movie', (err,rows)=>{
+		if(err) throw err;
+		if(rows.length){
+			responseData.result = 1;
+			responseData.data = rows;
+		}else{
+			responseData.result=0;
+		}
+		res.json(responseData);
+	});
+})
+
 
 module.exports = router;
